23d7208749fdbe699c5f1837bd5cbcd5ee11402d
David Arinder <david.arinder@gmail.com>
2023-11-15 10:35:35 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { createServer } from "node:http";
+import { schema } from "./schema";
+import { createYoga } from "graphql-yoga";
+
+const port = Number(process.env.API_PORT) || 4000;
+
+const yoga = createYoga({ schema });
+
+const server = createServer(yoga);
+
+server.listen(port, () => {
+  console.info(
+    `Server is running on http://localhost:${port}${yoga.graphqlEndpoint}`
+  );
+});
